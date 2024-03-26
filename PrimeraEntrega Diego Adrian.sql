28e4a3ac7d1fa168b25e4dbb31abef8426974eaf
Diego Adrian <diegojadrian97@gmail.com>
2024-03-26 14:06:14 -0300

    Update PrimeraEntrega Diego Adrian.sql

--- PrimeraEntrega Diego Adrian.sql
+++ PrimeraEntrega Diego Adrian.sql
@@ -25,6 +25,11 @@ CREATE TABLE Proveedor (
 COMMENT "Contiene información del Proveedor que le vende productos al negocio"
 ;
 
+CREATE TABLE Productos_Proveedores (
+    id_producto_proveedor INT AUTO_INCREMENT PRIMARY KEY
+)
+COMMENT "Tabla para manejar la relación muchos a muchos entre la tabla Productos y Proveedores"
+;
 
 
 CREATE TABLE Producto(
@@ -64,28 +69,53 @@ CREATE TABLE Ventas (
 COMMENT "Contiene información de las ventas de Productos que son realizadas por los Empleados a los Clientes "
 ;
 
+CREATE TABLE Detalle_Venta (
+    id_detalle_venta INT AUTO_INCREMENT PRIMARY KEY,
+    cantidad INT,
+    precio_unitario DECIMAL(10, 2),
+    total_producto DECIMAL(10, 2)
+)
+COMMENT "Tabla para registrar los detalles especificos de cada venta"
+;
+
+CREATE TABLE Inventario (
+    id_inventario INT AUTO_INCREMENT PRIMARY KEY,
+    cantidad_disponible INT
+)
+COMMENT "Tabla asignada al manejo de inventario y las cantidades de cada producto"
+;
+
 
 -- Se agregan las FK
 -- FK Proveedor
 ALTER TABLE Proveedor
 ADD COLUMN id_negocio INT,
 ADD FOREIGN KEY (id_negocio) REFERENCES Negocio(id_negocio);
+
+-- FK Producto Proveedor
+ALTER TABLE Productos_Proveedores
+ADD COLUMN id_producto INT,
+ADD COLUMN id_proveedor INT,
+ADD FOREIGN KEY (id_producto) REFERENCES Producto(id_producto),
+ADD FOREIGN KEY (id_proveedor) REFERENCES Proveedor(id_proveedor);
 -- FK Producto
 ALTER TABLE Producto
 ADD COLUMN id_negocio INT,
 ADD COLUMN id_proveedor INT,
 ADD FOREIGN KEY (id_negocio) REFERENCES Negocio(id_negocio),
 ADD FOREIGN KEY (id_proveedor) REFERENCES Proveedor(id_proveedor);
--- FK Clientes
-ALTER TABLE Clientes
-ADD COLUMN id_producto INT,
-ADD FOREIGN KEY (id_producto) REFERENCES Producto(id_producto);
 
 -- FK Empleados
 ALTER TABLE Empleados
 ADD COLUMN id_negocio INT,
 ADD FOREIGN KEY (id_negocio) REFERENCES Negocio(id_negocio);
 
+-- FK DETALLE DE VENTAS
+ALTER TABLE Detalle_Venta
+ADD COLUMN id_venta INT,
+ADD COLUMN id_producto INT,
+ADD FOREIGN KEY (id_venta) REFERENCES Ventas(id_venta),
+ADD FOREIGN KEY (id_producto) REFERENCES Producto(id_producto);
 -- FK Ventas
 ALTER TABLE Ventas
 ADD COLUMN id_negocio INT,
@@ -98,5 +128,10 @@ ADD COLUMN id_empleado INT,
 ADD FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado);
 ALTER TABLE Ventas
 ADD COLUMN id_producto INT,
-ADD FOREIGN KEY (id_producto) REFERENCES Producto(id_producto)
+ADD FOREIGN KEY (id_producto) REFERENCES Producto(id_producto);
 
+
+-- FK Inventario
+ALTER TABLE Inventario
+ADD COLUMN id_producto INT,
+ADD FOREIGN KEY (id_producto) REFERENCES Producto(id_producto)
